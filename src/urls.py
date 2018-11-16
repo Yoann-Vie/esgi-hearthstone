cdac6fb1c94da5863766a21c0b57facce1e060b3
oqomiz <jeremy.louis@cegetel.net>
2018-11-16 20:27:32 +0100

    add user and Log in

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
+from django.contrib import admin
+
+
 urlpatterns = [
     path('', views.home, name='home'),
+]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
