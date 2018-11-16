54a12f8763863195b1601f635849124e1d04731f
Yoann <contact@yoannvie.fr>
2018-11-16 22:45:53 +0100

    models : create cards and decks models

--- src/models.py
+++ src/models.py
@@ -1,2 +1,33 @@
 from django.db import models
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Deck(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
+    img = models.CharField(max_length=100, null=True, blank=True)
+    attack = models.IntegerField(null=True, blank=True)
+    type = models.CharField(max_length=100, null=True, blank=True)
+    health = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CardUser(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CardDeck(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
