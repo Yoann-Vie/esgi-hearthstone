0872db1e2db67f7fb0ed7b32f6c66dc321771e01
Yoann <contact@yoannvie.fr>
2018-11-16 23:48:59 +0100

    cards : add feature to get new cards

--- src/urls.py
+++ src/urls.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('get-cards', views.getCards, name='getCards'),
 ]
 
 #Add Django site authentication urls (for login, logout, password management)
--- src/views.py
+++ src/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib import messages
+from random import randint
+from .models import Card, CardUser
 
 def home(request):
-    title = 'Home'
-    context = {
-        'title': title
-    }
+    return render(request, 'hearthstone/index.html', context = { 'title': 'Accueil' })
 
-    return render(request, 'hearthstone/index.html', context)
+def getCards(request):
+    earned_cards = []
+    cards_count = Card.objects.all().count()
+    if request.user.is_authenticated:
+        for i in range(6):
+            random_index = randint(0, cards_count - 1)
+            card = Card.objects.all()[random_index]
+            earned_cards.append(card)
+            cardUser = CardUser(card=card, user=request.user)
+            cardUser.save()
+    else:
+        messages.warning(request, f'Vous devez être connecté pour obtenir des cartes')
+        return redirect('home')
+
+    return render(request, 'hearthstone/get-cards.html', { 'title': 'Nouvelles cartes', 'pack_cards': earned_cards })
